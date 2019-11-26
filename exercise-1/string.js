d398a6405e1d4ce83875061b1539d4452827b2d8
Raphael Fernandes <Raph@MacBook-Pro-de-Raphael.local>
2019-11-26 16:47:52 +0100

    camelCase

---
+++ exercise-1/string.js
@@ -0,0 +1,39 @@
+function ucfirst(str) {
+    if (typeof str !== "string" || !str) return '';
+
+    return str[0].toUpperCase() + str.substring(1);
+}
+
+console.log(ucfirst("test"));
+console.log(ucfirst("Test"));
+console.log(ucfirst(" test"));
+console.log(ucfirst("test Test tst"));
+console.log(ucfirst(""));
+console.log(ucfirst(null));
+console.log(ucfirst({}));
+
+function capitalize(str) {
+    if (typeof str !== "string" || !str) return '';
+
+
+    return str.toLowerCase().split(" ").map(function(item) {
+        return ucfirst(item);
+    }).join(" ");
+
+}
+
+console.log(capitalize("test"));
+console.log(capitalize("Test"));
+console.log(capitalize(" test"));
+console.log(capitalize("test Test tst"));
+console.log(capitalize(""));
+console.log(capitalize(null));
+console.log(capitalize({}));
+
+function camelCase(str) {
+    if (typeof str !== "string" || !str) return '';
+
+    return capitalize(str).split(" ").join("");
+    }
+
+console.log(camelCase("Je suis le TesT"));
